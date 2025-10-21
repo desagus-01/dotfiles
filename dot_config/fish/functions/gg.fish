1179090576004accffa5805c0ad80d8a92cb51cd
Gustavo de Sa <gugafera2016@outlook.com>
2025-10-21 16:18:57 -0300

    fixing gg

--- dot_config/fish/functions/gg.fish
+++ dot_config/fish/functions/gg.fish
@@ -1,12 +1,6 @@
 function gg -d "Add all file, commits and pushes to repo"
 
     git add .
-
-    if test (git diff --cached --quiet) -a (git diff --quiet)
-        echo "No changes to commit."
-        return 0
-    end
-
     git commit -m "$argv"
     git push
 
