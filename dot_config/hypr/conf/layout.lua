0e385469d5442a96b3c1d48117f0cf0e38ac3840
desagus <gugafera2016@outlook.com>
2026-05-13 16:32:35 +0100

    updates to colour of borders in hypr and scrolling for pc

--- dot_config/hypr/conf/layout.lua
+++ dot_config/hypr/conf/layout.lua
@@ -1,5 +1,6 @@
 hl.config({
 	dwindle = { preserve_split = true },
+	scrolling = { column_width = 1 },
 	binds = {
 		workspace_back_and_forth = true,
 		allow_workspace_cycles = true,
--- dot_config/hypr/conf/windows.lua
+++ dot_config/hypr/conf/windows.lua
@@ -1,13 +1,13 @@
 local globals = require("hypr_globals")
 hl.config({
 	general = {
-		gaps_in = 4,
-		gaps_out = 5,
+		gaps_in = 5,
+		gaps_out = 7,
 		border_size = 3,
 		layout = "dwindle",
 		resize_on_border = true,
 		col = {
-			active_border = globals.secondary_color,
+			active_border = globals.primary_fixed,
 			inactive_border = globals.on_secondary_color,
 		},
 	},
